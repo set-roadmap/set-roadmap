e9d55c29cddb5a695e204d6140a1494f178d7989
Aashish Upadhyay <boysmall300@gmail.com>
2024-07-01 01:20:18 +0545

    welcome pages

---
+++ src/app/welcome/create-team/page.tsx
@@ -0,0 +1,9 @@
+import CreateTeamForm from "@/components/shared/create-team-form";
+
+export default function WelcomeCreateTeam() {
+  return (
+    <div>
+      <CreateTeamForm />
+    </div>
+  );
+}
--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/primitives/button";
 import { headingFont } from "@/lib/heading-font";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function WelcomePage() {
   return (
-    <div className="h-full w-full flex flex-col gap-8 items-center justify-center">
+    <div className="space-y-8 text-center">
       <h3
         className={cn(
           "text-5xl font-bold tracking-wide",
@@ -12,11 +13,13 @@ export default function WelcomePage() {
         )}>
         Welcome to PonyMap
       </h3>
-      <p className="w-[28rem] text-center text-lg ">
+      <p className="w-[30rem] text-lg ">
         PonyMap gives you the power to create personalized roadmaps and gives
         you the all the tracking metrics.
       </p>
-      <Button size={"lg"}>Get Started</Button>
+      <Button size={"lg"} asChild>
+        <Link href="/welcome/create-team">Get Started</Link>
+      </Button>
     </div>
   );
 }
